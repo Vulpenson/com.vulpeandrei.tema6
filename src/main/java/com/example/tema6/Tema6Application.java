9acd8e5dccd959f916631c1304ed12becf51e752
Andrei Vulpe <andreivulpe02@yahoo.com>
2022-08-09 14:52:34 +0300

    commit
    initial commit

---
+++ src/main/java/com/example/tema6/Tema6Application.java
@@ -0,0 +1,13 @@
+package com.example.tema6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tema6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tema6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tema6/Tema6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tema6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tema6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
